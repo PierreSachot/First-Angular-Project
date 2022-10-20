40fc0040487520054da82f54da77f0c294d33833
Pierre Sachot <sachot.pierre@laposte.net>
2022-10-20 17:51:08 +0200

    added pokemon selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { POKEMONS } from './moke-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title: string = `Ma première application`;
+export class AppComponent implements OnInit{
+  title: string = "Liste de pokémons";
+  pokemonList : Pokemon[] = POKEMONS;
+  selectedPokemon : Pokemon;
+
+  ngOnInit() {
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon : Pokemon){
+    this.selectedPokemon = pokemon;
+  }
+
 }
