dba8a289fb78f3650b7969ae7c464ca3cabc167c
Pierre Sachot <sachot.pierre@laposte.net>
2022-10-20 11:14:49 +0200

    Removed unused html, changed tsconfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'First-Angular-Project';
+  title: string = `Ma première application`;
 }
