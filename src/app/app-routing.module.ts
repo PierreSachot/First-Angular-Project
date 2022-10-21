53f429bba9822fdc93fee4fa88347d756bbc66de
Pierre Sachot <sachot.pierre@laposte.net>
2022-10-21 17:07:12 +0200

    Module refacto now working properly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 const routes: Routes = [
-  { path:'pokemons', component: PokemonListComponent },
-  { path:'pokemons/:id', component: PokemonDetailsComponent},
   { path:'', redirectTo: 'pokemons', pathMatch: 'full' },
   { path:'**', component: PageNotFoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
-import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
-import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    PokemonListComponent,
-    PokemonCardComponent,
-    PokemonDetailsComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
